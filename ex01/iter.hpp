c803006cc14d05a4a9a5e2b3490cc189fe5e5af8
Genaro Garibotti <ggaribot@paul-f3Br5s3.clusters.42paris.fr>
2025-04-17 17:33:47 +0200

    done

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -6,7 +6,8 @@
 template <typename T>
 void iter(T* array, size_t length, void (*func)(T const &))
 {
-    for (size_t i = 0; i < length; i++) {
+    for (size_t i = 0; i < length; i++)
+    {
         func(array[i]);
     }
 }
