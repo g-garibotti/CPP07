322627982398df49f7b272a79fef335dc62a008d
Genaro Garibotti <ggaribot@bess-f2r4s3.clusters.42paris.fr>
2025-04-18 16:00:32 +0200

    after correction iter

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -3,8 +3,8 @@
 
 #include <cstddef>
 
-template <typename T>
-void iter(T* array, size_t length, void (*func)(T const &))
+template <typename T, typename F>
+void iter(T* array, size_t length, F func)
 {
     for (size_t i = 0; i < length; i++)
     {
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -2,7 +2,7 @@
 #include "iter.hpp"
 
 template <typename T>
-void print(T const &x)
+void print(T& x)
 {
     std::cout << x << " ";
 }
@@ -11,17 +11,17 @@ int main()
 {
     int intArr[] = {1, 2, 3, 4, 5};
     std::cout << "Int array: ";
-    iter(intArr, 5, print);
+    iter(intArr, 5, print<const int>);
     std::cout << std::endl;
     
     char charArr[] = {'a', 'b', 'c', 'd', 'e'};
     std::cout << "Char array: ";
-    iter(charArr, 5, print);
+    iter(charArr, 5, print<char>);
     std::cout << std::endl;
 
     double doubleArr[] = {1.1, 2.2, 3.3};
     std::cout << "Double array: ";
-    iter(doubleArr, 3, print);
+    iter(doubleArr, 3, print<const double>);
     std::cout << std::endl;
     
     return 0;
